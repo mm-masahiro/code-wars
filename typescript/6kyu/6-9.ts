96fbc9c02cb1399d5a3069182e896676578e22df
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-07-05 22:55:09 +0900

    minimum code

---
+++ typescript/6kyu/6-9.ts
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/54da5a58ea159efa38000836/train/typescript
+// my solution
+
+export const findOdd = (xs: number[]): number => {
+	//   数値を1つずつarrayに入れていって、ダブりが出てきたらその要素をarrayから削除する
+	//   最後残ったものをreturnする
+		if (xs.length === 1) return xs[0]
+		
+		return 0;
+	};
+	

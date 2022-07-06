0d65ad2e376c7398b3d4181d14be77e7d73e766e
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-07-06 20:35:16 +0900

    my solution

---
+++ typescript/5kyu/5-10.ts
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5270f22f862516c686000161/train/typescript
+// my solution
+export function toBase64(str: string): string {
+  return Buffer.from(str).toString('base64')
+}
+
+export function fromBase64(str: string): string {
+  return Buffer.from(str, 'base64').toString()
+}

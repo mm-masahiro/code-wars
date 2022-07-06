7ba06b2cf775407daebf4509ef8e69717a74c48a
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-07-06 21:35:08 +0900

    best solution

--- typescript/5kyu/5-11.ts
+++ typescript/5kyu/5-11.ts
@@ -1,14 +1,27 @@
 // https://www.codewars.com/kata/51e04f6b544cf3f6550000c1/train/typescript
 // my solution
-export function beeramid(bonus: number, price: number): number {
-	const purchasedBeer = bonus / price;
-	let towerHeight = 1;
-	let usedCan = 0;
-	let needCan = 0;
+// export function beeramid(bonus: number, price: number): number {
+// 	const purchasedBeer = bonus / price;
+// 	let towerHeight = 1;
+// 	let usedCan = 0;
+// 	let needCan = 0;
 
-	for(towerHeight; usedCan <= purchasedBeer; towerHeight ++) {
-		needCan = towerHeight * towerHeight;
-		usedCan = usedCan + needCan;
-	}
-	return towerHeight
+// 	for(towerHeight; usedCan <= purchasedBeer; towerHeight ++) {
+// 		needCan = towerHeight * towerHeight;
+// 		usedCan = usedCan + needCan;
+// 	}
+// 	return towerHeight
+// }
+
+// best solution
+export function beeramid(bonus: number, price: number): number {
+  let level: number = 0;
+  let n: number = 1;
+  let i: number = 1
+  while(n <= Math.floor(bonus / price)){
+    i++;
+    n += i ** 2;
+    level++
+  }
+  return level
 }

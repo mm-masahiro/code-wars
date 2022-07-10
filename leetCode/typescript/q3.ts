5d243ba3980aa004535c1d7d55b15c81cb446e8d
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-07-10 18:15:32 +0900

    ts q3

---
+++ leetCode/typescript/q3.ts
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/minimum-distance-to-the-target-element/
+// my code
+function getMinDistance(nums: number[], target: number, start: number): number {
+	const targetIndex = nums.flatMap((num, i) => num === target ? i : []);
+	if (targetIndex.length >= 2) {
+		const min = targetIndex.reduce((ti, num, i) => {
+			return Math.abs(ti - start) < Math.abs(num - start) ? ti : num
+		})
+		return Math.abs(min - start)
+	}
+	return Math.abs(targetIndex[0] - start);
+};

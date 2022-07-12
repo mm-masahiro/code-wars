b2ef0b14e118dd1fe700ad4907d6a818185637df
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-07-12 23:09:10 +0900

    best coed

--- leetCode/typescript/q5.ts
+++ leetCode/typescript/q5.ts
@@ -9,3 +9,15 @@ function runningSum(nums: number[]): number[] {
 	})
 	return result
 };
+
+// best code
+// function runningSum(nums: number[]): number[] {
+// 	let res: number[] = [];
+// 	if(nums.length == 0) return res;
+// 	res[0] = nums[0];
+// 	for(let i = 1; i < nums.length; i++) {
+// 		res[i] = res[i - 1] + nums[i];
+// 	}
+	
+// 	return res;
+// };

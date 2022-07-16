46f0448e7a2ec1606aed1860f9d6554d2417dc0f
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-07-16 10:24:27 +0900

    ruby best

--- leetCode/ruby/q1.rb
+++ leetCode/ruby/q1.rb
@@ -1,7 +1,15 @@
+# https://leetcode.com/problems/root-equals-sum-of-children/
+# my code
+
 def check_tree(root)
 	sum = root.left.val + root.right.val
 	rootNode = root.val
 	
 	return true if rootNode == sum
 	false
+end
+
+# best code
+def check_tree(root)
+	root.val == root.left.val + root.right.val
 end

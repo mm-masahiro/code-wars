18a8aa37fc7e35ee6aab7f2e293b741d2fe70a6a
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-07-17 23:21:08 +0900

    q7 ts my code

---
+++ leetCode/typescript/q7.ts
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/defanging-an-ip-address/submissions/
+// my code
+function defangIPaddr(address: string): string {
+	const regex = /\./g;
+	return address.replace(regex, "[.]");
+};

76aa72b27bfe05d99197080741760a4d886eaee4
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-08-10 22:50:52 +0900

    set counter

--- leetCode/typescript/q9.js
+++ leetCode/typescript/q9.js
@@ -2,15 +2,17 @@
 function twoSum(nums, target) {
     var result = [];
     for (var i = 0; i < nums.length - 1; i++) {
+        var baseCounter = i;
         for (var j = i + 1; j < nums.length; j++) {
-            var counter = i;
-            if (nums[i] + nums[j] >= target)
-                return result = [i, j];
-            console.log(i, j);
+            var counter = j;
+            console.log(baseCounter);
+            if (nums[baseCounter] + nums[counter] >= target)
+                return result = [baseCounter, counter];
             // ↑の答えがない時は、i+1, i+2をしたい
             // nums[i+1] + nums[j]
             // ループの回数をカウントしてる変数があれば良い？ → j??
             // if (nums[j] + nums[j+1] >= target) return result = [j, j+1]
+            baseCounter++;
             counter++;
         }
     }
@@ -18,6 +20,7 @@ function twoSum(nums, target) {
 }
 ;
 console.log(twoSum([3, 2, 3], 6));
+// expected [1,2]
 // best
 // const twoSum = (nums, target) => {
 //   const map = {};
--- leetCode/typescript/q9.ts
+++ leetCode/typescript/q9.ts
@@ -3,14 +3,16 @@
 function twoSum(nums: number[], target: number): number[] {
 	let result: number[] = [];
 	for (let i = 0; i < nums.length - 1; i++) {
+		let baseCounter = i;
 		for (let j = i + 1; j < nums.length; j++) {
-			let counter = i;
-			if (nums[i] + nums[j] >= target) return result = [i, j]
-			console.log(i, j)
+			let counter = j;
+			console.log(baseCounter)
+			if (nums[baseCounter] + nums[counter] >= target) return result = [baseCounter, counter]
 			// ↑の答えがない時は、i+1, i+2をしたい
 			// nums[i+1] + nums[j]
 			// ループの回数をカウントしてる変数があれば良い？ → j??
 			// if (nums[j] + nums[j+1] >= target) return result = [j, j+1]
+			baseCounter++
 			counter++
 		}
 	}
@@ -18,6 +20,7 @@ function twoSum(nums: number[], target: number): number[] {
 };
 
 console.log(twoSum([3,2,3], 6))
+// expected [1,2]
 
 
 // best

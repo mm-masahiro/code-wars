510ff17a44452b12b3da1ae23b7f2200c43cbbe2
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-08-12 08:58:05 +0900

    my solution

--- leetCode/typescript/q9.ts
+++ leetCode/typescript/q9.ts
@@ -1,23 +1,38 @@
 // https://leetcode.com/problems/two-sum/submissions/
 
 function twoSum(nums: number[], target: number): number[] {
-	const map = {};
-
-	for (let i = 0; i < nums.length; i++) {
-		const another = target - nums[i]
-		if (another in map) {
-			return [map[another], i]
+	let result: number[] = [];
+	for (let i = 0; i < nums.length - 1; i++) {
+		let baseCounter = 0;
+		let counter = i + 1;
+		for (let j = i + 1; j < nums.length; j++) {
+			console.log(baseCounter, counter)
+			if (nums[baseCounter] + nums[counter] === target) return result = [baseCounter, counter]
+			baseCounter++
+			counter++
 		}
-		map[nums[i]] = i
 	}
-	return [];
+	return result
 };
 
 const nums: number[] = [0,4,3,0]
-// const nums: number[] = [2,7,11,15]
-// const nums: number[] = [3, 2, 3]
 
 const target: number = 0
-// const target: number = 9
-// const target: number = 6
 console.log(twoSum(nums, target))
+
+// best
+// const twoSum = (nums, target) => {
+//   const map = {};
+
+//   for (let i = 0; i < nums.length; i++) {
+//     const another = target - nums[i];
+
+//     if (another in map) {
+//       return [map[another], i];
+//     }
+
+//     map[nums[i]] = i;
+//   }
+
+//   return null;
+// };

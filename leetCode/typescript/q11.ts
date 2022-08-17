66057ce3f95881d0366131a744fa3ab748689ea7
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-08-17 22:32:52 +0900

    fix

--- leetCode/typescript/q11.ts
+++ leetCode/typescript/q11.ts
@@ -1,22 +1,22 @@
 function isValid(s: string): boolean {
 	const devidedStrings = s.split("")
 	const parenthesesPairs = {
-			'(': ')',
-			'[': ']',
-			'{': '}',
+		'(': ')',
+		'[': ']',
+		'{': '}',
 	}
 	const parentheses = [];
 	
 	if (devidedStrings.length === 1) return false
 	
 	for(let i = 0; i < devidedStrings.length; i++) {
-			if (devidedStrings[i] === '(' || devidedStrings[i] === '[' || devidedStrings[i] === '{') {
-					parentheses.push(devidedStrings[i])
-			};
-			if (devidedStrings[i] === ')' || devidedStrings[i] === ']' || devidedStrings[i] === '}') {
-					const poppedString = parentheses.pop()
-					if (parenthesesPairs[poppedString] !== devidedStrings[i]) return false
-			}
+		if (devidedStrings[i] === '(' || devidedStrings[i] === '[' || devidedStrings[i] === '{') {
+			parentheses.push(devidedStrings[i])
+		};
+		if (devidedStrings[i] === ')' || devidedStrings[i] === ']' || devidedStrings[i] === '}') {
+			const poppedString = parentheses.pop()
+			if (parenthesesPairs[poppedString] !== devidedStrings[i]) return false
+		}
 	}
 	
 	return parentheses.length === 0 ? true : false

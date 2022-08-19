f62960c2f75d6f8a01667e91084dde0aedb83a74
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-08-19 22:30:04 +0900

    in the middle

---
+++ leetCode/typescript/q13.ts
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+class KthLargest {
+	constructor(k: number, nums: number[]) {
+		this.target = k;
+		this.elements = nums;
+	}
+
+	target: number;
+	elements: number[]
+
+	add(val: number): number {
+		this.elements.push(val)
+			
+		for (let i = 0; i < this.elements.length - 1; i++) {
+			for (let j = 0; j < this.elements.length - 1; j++) {
+				if (this.elements[j] < this.elements[j + 1]) {
+						const temp = this.elements[j];
+						this.elements[j] = this.elements[j + 1];
+						this.elements[j + 1] = temp;
+				}
+			}   
+		}
+		return this.elements[this.target - 1]
+	}
+}
+
+/**
+* Your KthLargest object will be instantiated and called as such:
+* var obj = new KthLargest(k, nums)
+* var param_1 = obj.add(val)
+*/
+

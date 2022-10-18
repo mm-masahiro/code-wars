d5f23988a5b9f1c42dfcd9a28b97d073aa6a3105
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-10-19 00:24:01 +0900

    LongestCommonPrefix

--- leetCode/typescript/Easy/LongestCommonPrefix/myCode/longestCommonPrefix.ts
+++ leetCode/typescript/Easy/LongestCommonPrefix/myCode/longestCommonPrefix.ts
@@ -1,5 +1,21 @@
 // https://leetcode.com/problems/longest-common-prefix/
 
 function longestCommonPrefix(strs: string[]): string {
-	const prefix = strs[0][0];
+  // strs[0][0]を取得
+  // strs[1][0], strs[2][0], strs[3][0]...と比較して、全て値が同じであれば同じであれば文字列に追加していく
+
+  // strs[0][1]を取得
+  // strs[1][1], strs[2][1], strs[3][1]...と比較して、全て値が同じであれば同じであれば文字列に追加していく
+
+  // ↑を終わるまで or 異なる値が来るまで繰り返す
+	let commonPrefix: string = "";
+
+  for (let i = 0; i < strs[0].length; i++) {
+    const base = strs[0][i];
+    for (let j = 1; j < strs.length; j++) {
+      if (base !== strs[j][i]) return commonPrefix
+    }
+    commonPrefix += base;
+  }
+  return commonPrefix;
 };

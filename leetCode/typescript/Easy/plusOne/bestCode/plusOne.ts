53591cd2071e4d6c693fd811493653f71b041725
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-10-22 11:07:15 +0900

    plusOne/bestCode

---
+++ leetCode/typescript/Easy/plusOne/bestCode/plusOne.ts
@@ -0,0 +1,10 @@
+function plusOne(digits) {
+  let co = true;
+  for (let i = digits.length - 1; i >= 0 && co; i--) {
+    digits[i]++;
+    co = digits[i] >= 10;
+    co && (digits[i] = 0);
+  }
+  co && digits.unshift(1);
+  return digits;
+}
